f3ef9660cc5cbe68e7aa735152bcb9e98039586e
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-08 16:17:17 -0300

    props e children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
-      <p>Praticando React</p>
+      <Banner image={"home"} />
+      <Container>
+        <h1>Hello world</h1>
+        <p>Praticando React</p>
+      </Container>
       <Footer />
     </>
   )
