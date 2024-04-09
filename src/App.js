e1da25f0dccd9ebe9c4bd7f3e14ad6cd49ca2a1d
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-09 16:13:37 -0300

    Refatoração e Rotas
    Criação de rotas e refatoração de códigos para diminuir repetições e ajustar rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Banner from "./components/Banner";
-import Container from "./components/Container";
-import Card from "./components/Card";
-import Category, { categories, filterCategory } from "./components/Category";
+import AppRoutes from "./routes";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Banner image={"home"} />
-      <Container>
 
-        { categories.map((category, index) =>
-          <Category category={category}>
-            { filterCategory(index).map((video) => <Card id={video.id} key={video.id} /> )}
-          </Category>
-        )}
-        
-      </Container>
-      <Footer />
-    </>
+  return (
+    <AppRoutes />
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom"
 import styles from "./Header.module.css"
 
 function Header() {
     return (
         <header className={styles.header}>
-            <span>ReactFlix</span>
+            <Link to="/">
+                <span>ReactFlix</span>
+            </Link>
             <nav>
-                <a href="#">Home</a>
-                <a href="#">Assistir</a>
+                <Link to="/">Home</Link>
+                <Link to="/Watch">Assistir</Link>
             </nav>
         </header>
     )
