e9198169649d15e588b6adb3de8e2d4207584b68
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-09 11:29:07 -0300

    map-export-import-scope
    Solução de mapeamento para retirar repetição de códigos e solução para problemas de escopo, por questão de organização da const categories e function filterCategory

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import videos from "./json/videos.json"
-import Category from "./components/Category";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Metereologia, Vegetação",
-  "Geologia e Hidrografia"
-]
-
-function filterCategory(id) {
-  return videos.filter( video => video.category === categories[id] )
-}
+import Category, { categories, filterCategory } from "./components/Category";
 
 function App() {
   return (
@@ -25,21 +12,11 @@ function App() {
       <Banner image={"home"} />
       <Container>
 
-        <Category category={categories[0]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-        <Category category={categories[1]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-        <Category category={categories[2]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-        <Category category={categories[3]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
-        <Category category={categories[4]}>
-          { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
-        </Category>
+        { categories.map((category, index) =>
+          <Category category={category}>
+            { filterCategory(index).map((video) => <Card id={video.id} key={video.id} /> )}
+          </Category>
+        )}
         
       </Container>
       <Footer />
