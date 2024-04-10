b73a2a4bed4e40a287c646bd20de176db5f23c08
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-10 15:49:40 -0300

    Controle de estados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,32 @@ import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Card from "../../components/Card";
 import Category, { categories, filterCategory } from "../../components/Category";
+import { useState } from "react";
 
 function Home() {
+
+  // let nomeDigitado = ''
+  function pegarNome(event) {
+    console.log(event.target.value)
+    // nomeDigitado = event.target.value
+    setNome(event.target.value)
+  }
+
+  // Hook: useState
+  const [ nome, setNome ] = useState('');
+ 
   return (
     <>
       <Header />
       <Banner image={"home"} />
       <Container>
+
+        <input 
+          type="text"
+          placeholder="Nome"
+          onChange={pegarNome}  
+        />
+        <h2>{nome}</h2>
 
         { categories.map((category, index) =>
           <Category category={category}>
