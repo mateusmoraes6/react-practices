6076f963181c277e319f060d9d124dbb279a6e25
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-12 10:26:43 -0300

    Page VideoRegister

---
+++ src/components/Form/index.js
@@ -0,0 +1,34 @@
+import { categories } from "../Category";
+import styles from "./Form.module.css"
+
+function Form() {
+    return (
+        <section className={styles.container}>
+            <h2>Cadastro de vídeo</h2>
+            <form>
+                <div>
+                    <label>URL do vídeo</label>
+                    <input 
+                        type="text"
+                        placeholder="Adicione a URL do vídeo..."
+                        required="required"
+                    />
+                </div>
+                <div>
+                    <label>Categoria</label>
+                    <select>
+                        <option value="-">Selecione uma categoria</option>
+                        { categories.map(item => {
+                            return <option value={item}>{item}</option>
+                        }) }
+                    </select>
+                </div>
+                <div>
+                    <button>Cadastrar</button>
+                </div>
+            </form>
+        </section>
+    );
+}
+
+export default Form;
---
+++ src/pages/VideoRegister/index.js
@@ -0,0 +1,18 @@
+import Header from "../../components/Header"
+import Container from "../../components/Container"
+import Form from "../../components/Form"
+import Footer from "../../components/Footer"
+
+function VideoRegister() {
+    return (
+        <>
+            <Header />
+            <Container>
+                    <Form />
+            </Container>
+            <Footer />
+        </>
+    );
+}
+
+export default VideoRegister;
