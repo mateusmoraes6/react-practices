2f78d0acfcb9a6956449b9dc017b366e4dcd5354
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-04-12 14:43:38 -0300

    useState e useEffect
    Renderização Condicional

--- src/components/SearchVideoList/index.js
+++ src/components/SearchVideoList/index.js
@@ -1,6 +1,7 @@
 import styles from "./SearchVideoList.module.css";
 import VideoList from "../../components/VideoList"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Loader from "../Loader";
 
 // Filtrando videos por categoria
 function filterVideos(videos, searchText) {
@@ -12,6 +13,11 @@ function SearchVideoList({ videos }) {
     const [ searchText, setSearchText ] = useState('')
     const foundVideos = filterVideos(videos, searchText)
 
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 900)
+    }, []);
+
     return (
         <sectiom className={styles.container}>
             <input 
@@ -20,10 +26,12 @@ function SearchVideoList({ videos }) {
                 value={searchText}
                 onChange={event => setSearchText(event.target.value)}
             />
-            <VideoList 
-                videos={foundVideos}
-                emptyHeading={`Sem vídeos sobre "${searchText}"`}
-            />
+            { loading ? <Loader /> :
+                <VideoList 
+                    videos={foundVideos}
+                    emptyHeading={`Sem vídeos sobre "${searchText}"`}
+                />
+            }
         </sectiom>
     );
 }
